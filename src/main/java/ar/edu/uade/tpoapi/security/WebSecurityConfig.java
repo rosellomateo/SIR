b75180145e0c647d4f75e6abf7776199eaad484f
CAFM <carlosfernandezmendez091@gmail.com>
2023-09-23 11:32:55 -0300

    security

---
+++ src/main/java/ar/edu/uade/tpoapi/security/WebSecurityConfig.java
@@ -0,0 +1,8 @@
+package ar.edu.uade.tpoapi.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebSecurityConfig{
+}
