1cafd46c86780722d9f8933d2bd9646c87ba0e13
CAFM <carlosfernandezmendez091@gmail.com>
2023-11-06 18:05:28 -0300

    Add comments to Reclamo class and update ReclamoView to include comments

---
+++ src/main/java/ar/edu/uade/tpoapi/modelo/Comentario.java
@@ -0,0 +1,43 @@
+package ar.edu.uade.tpoapi.modelo;
+
+import java.util.Date;
+
+import ar.edu.uade.tpoapi.views.ComentarioView;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "comentarios")
+public class Comentario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idcomentario;
+    private String texto;
+    private Date fecha;
+    private Persona usuario; 
+    private String urlImagen;    
+
+    public ComentarioView toView()
+    {
+        return ComentarioView.builder()
+            .idcomentario(this.idcomentario)
+            .texto(this.texto)
+            .fecha(this.fecha.toString())
+            .persona(this.usuario.toView())
+            .urlImagen(this.urlImagen)
+            .build();
+    }
+}
